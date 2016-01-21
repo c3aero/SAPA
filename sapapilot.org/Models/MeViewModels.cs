2369c85d7bdaec7c8e6982a6ff3a5d846f495b67
John Speranza <john@c3aero.com>
2016-01-21 17:19:25 -0500

    Added user profile fields to SAPA.Models.GetViewModel (MeViewModels.cs)

--- sapapilot.org/Models/MeViewModels.cs
+++ sapapilot.org/Models/MeViewModels.cs
@@ -8,5 +8,16 @@ namespace SAPA.Models
     public class GetViewModel
     {
         public string Hometown { get; set; }
+        public string FirstName { get; set; }
+        public int EmployeeID { get; set; }
+        public int Seniority { get; set; }
+        public DateTime DOH { get; set; }
+        public string VotingCode { get; set; }
+        public string Domicile { get; set; }
+        public string Position { get; set; }
+        public string Fleet { get; set; }
+        public DateTime BirthMonth { get; set; }
+        public DateTime SeniorityDate { get; set; }
+        public DateTime CheckrideDate { get; set; }
     }
 }
